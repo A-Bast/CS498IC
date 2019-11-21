b658a6afc1c54a18727e2c3273f461030ae45b56
A-Bast <49210515+A-Bast@users.noreply.github.com>
2019-11-21 17:39:20 -0500

    Add new branch

--- src/main/app.js
+++ src/main/app.js
@@ -8,6 +8,12 @@ app.get('/', (req, res) => {
     res.status(200).send('Hello World!')
 })
 
+// The code below will display 'Hello World!' to the browser when you go to http://localhost:3000
+app.get('/change', (req, res) => {
+    // the .status(200) isn't necessary since it is the default return status
+    res.status(200).send('Hello Planet!')
+})
+
 app.listen(3000, () => {
     console.log('Example app listening on http://localhost:3000')
 })
